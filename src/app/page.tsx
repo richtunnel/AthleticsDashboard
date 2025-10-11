8de6bbe47c2f787a4ec60e68bda6ec113ca2eb06
Rick Stokes <rickydottech@gmail.cmo>
2025-10-11 10:38:16 -0500

    [css]: home, login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <div style={{ height: "100vh", textAlign: "center" }} className="flex items-center text-center items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100">
-      <div style={{ textAlign: "center" }} className="text-center grid content-center">
-        <h1 className="text-5xl font-bold text-gray-900 mb-4">Athletic Director Dashboard</h1>
-        <p className="text-xl text-gray-600 mb-8">Manage your athletic schedules with ease</p>
-        <Link href="/api/auth/signin" className="inline-block px-8 py-3 bg-blue-600 text-white rounded-lg font-medium hover:bg-blue-700 transition">
-          Get Started
-        </Link>
+    <div className="grid h-screen grid-cols-[1fr_1.2fr] text-center">
+      <div className="relative h-full">
+        <Image src="/assets/images/green-energy.jpg" alt="Athletics Dashboard Illustration" fill className="object-cover" priority />
+      </div>
+      <div className="flex h-full items-center justify-center">
+        <div>
+          <h3 className="text-5xl font-bold text-gray-900 mb-4">
+            Athletic <br /> Directors Hub
+          </h3>
+          <p className="text-xl text-gray-600 mb-8">A smart spreadsheet allowing athletic directors to automate, process and manage athletic schedules with ease. </p>
+          <Link href="/api/auth/signin" style={{ backgroundColor: "#b4fc66", color: "#000" }} className="inline-block px-8 py-3 text-white rounded-lg font-medium transition">
+            Get Started
+          </Link>
+        </div>
       </div>
     </div>
   );
