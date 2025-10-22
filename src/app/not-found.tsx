f1a89f26457ed9ffc3a94fbcf6fe2a4f33959750
Rick Stokes <rickydottech@gmail.com>
2025-10-22 03:37:03 -0500

    dev update

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+// app/not-found.tsx
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <h1 className="text-3xl font-bold mb-2">404 - Page Not Found</h1>
+      <p className="text-gray-500">Sorry, we couldn’t find the page you’re looking for.</p>
+    </div>
+  );
+}
