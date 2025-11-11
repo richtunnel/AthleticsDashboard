fd19e09bba4c6f40f7bfa66c21b5c89e56bb08d6
Rick Stokes <rickydottech@gmail.com>
2025-11-11 09:57:16 -0600

    added mixpanel

---
+++ src/lib/analytics/gtag.ts
@@ -0,0 +1,17 @@
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || "G-XXXXXXX";
+
+// Send page views
+export const pageview = (url: string) => {
+  window.gtag("config", GA_MEASUREMENT_ID, {
+    page_path: url,
+  });
+};
+
+// Log specific events
+export const event = ({ action, category, label, value }: any) => {
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
