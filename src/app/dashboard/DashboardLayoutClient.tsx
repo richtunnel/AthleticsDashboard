a04adae7a39d6d590ceba3ee65e6fb19bdd04ac1
Rick Stokes <rickydottech@gmail.com>
2025-11-26 15:01:48 -0600

    opponents update

--- src/app/dashboard/DashboardLayoutClient.tsx
+++ src/app/dashboard/DashboardLayoutClient.tsx
@@ -70,13 +70,13 @@ const DRAWER_WIDTH = 240;
 const navigation = [
   { name: "Dashboard", href: "/dashboard", icon: DashboardIcon },
   { name: "Game Center", href: "/dashboard/games", icon: CalendarMonth },
-  { name: "Manage Teams", href: "/dashboard/opponents", icon: Groups },
+  { name: "Teams & Scores", href: "/dashboard/opponents", icon: Groups },
   { name: "My Calendars", href: "/dashboard/gsync", icon: EditCalendarIcon },
   { name: "Manage Emails", href: "/dashboard/email-groups", icon: EmailIcon },
   { name: "Email Logs", href: "/dashboard/email-logs", icon: HistoryIcon },
   // { name: "Analytics", href: "/dashboard/analytics", icon: Analytics },
   { name: "Settings", href: "/dashboard/settings", icon: Settings },
-  { name: "Travel AI", href: "/dashboard/travel-ai", icon: DepartureBoardIcon },
+  // { name: "Travel AI", href: "/dashboard/travel-ai", icon: DepartureBoardIcon },
 ];
 
 function DashboardLayoutContent({ children }: { children: React.ReactNode }) {
