53f7c65f6d5bec6b2b11f908bfb56a727414cff5
Rick Stokes <rickydottech@gmail.com>
2025-12-01 14:03:40 -0600

    fix/update: google api scopes

--- src/app/api/auth/calendar-connect/route.ts
+++ src/app/api/auth/calendar-connect/route.ts
@@ -4,8 +4,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/utils/authOptions";
 
 const SCOPES = [
-  "https://www.googleapis.com/auth/calendar.events",
-  "https://www.googleapis.com/auth/contacts.readonly",
+  // "https://www.googleapis.com/auth/calendar.events",
+  // "https://www.googleapis.com/auth/contacts.readonly",
   "https://www.googleapis.com/auth/userinfo.email",
 ];
 
