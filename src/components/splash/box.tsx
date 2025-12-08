7e98966ac2722dc65a07759a26e566a79ee0a67a
Rick Stokes <rickydottech@gmail.cmo>
2025-12-08 11:49:58 -0600

    update

---
+++ src/components/splash/box.tsx
@@ -0,0 +1,6 @@
+import { Box } from "@mui/material";
+import { motion } from "framer-motion";
+
+export const MotionBox = motion(Box);
+
+export type MotionBoxProps = React.ComponentProps<typeof MotionBox>;
