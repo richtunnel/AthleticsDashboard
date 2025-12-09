84a2632ed01f0e0dc0b7dd427cf90906f2cd76a2
Rick Stokes <rickydottech@gmail.com>
2025-12-08 19:46:54 -0600

    adhub

---
+++ src/components/copyright.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+interface CopyRightProps {
+  children?: React.ReactNode;
+}
+
+const CopyRight: React.FC<CopyRightProps> = ({ children }) => {
+  const defaultText = `© ${new Date().getFullYear()} Athletic Director Hub. All rights reserved.`;
+
+  return (
+    <Typography variant="body2" color="text.secondary" style={{ marginTop: "5px" }} sx={{ fontSize: "0.75rem", fontWeight: 400 }}>
+      {children || defaultText}
+    </Typography>
+  );
+};
+
+export default CopyRight;
