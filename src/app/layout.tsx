4ea0ed6c70ac271bd152c0b5953440875c6522c1
Rick Stokes <rickydottech@gmail.com>
2025-12-26 16:21:31 -0600

    api update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Built by Richard Stokes @ Visual Embassy */}
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
