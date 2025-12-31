20278853e080f641bbaf8bbfe2fac208025c5b51
Rick Stokes <rickydottech@gmail.com>
2025-12-31 14:15:39 -0600

    google api update

--- src/app/api/auth/calendar-connect/route.ts
+++ src/app/api/auth/calendar-connect/route.ts
@@ -5,7 +5,7 @@ import { authOptions } from "@/lib/utils/authOptions";
 
 const SCOPES = [
   // "https://www.googleapis.com/auth/calendar.events",
-  "https://www.googleapis.com/auth/calendar",
+  "https://www.googleapis.com/auth/calendar.events",
   // "https://www.googleapis.com/auth/contacts.readonly",
   "https://www.googleapis.com/auth/userinfo.email",
 ];
