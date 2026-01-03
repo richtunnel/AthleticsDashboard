4f0eb7d9f25eca39886002e59b5db925ccf0ed1b
Rick Stokes <rickydottech@gmail.cmo>
2026-01-03 17:32:53 -0600

    homepage content update

--- src/components/splash/SectionCard.tsx
+++ src/components/splash/SectionCard.tsx
@@ -64,7 +64,7 @@ export const ArcCard: React.FC = () => {
                 src="/assets/images/op-illustration-ipad-dark.png"
                 alt="Arc logo"
                 width={625}
-                height={685}
+                height={300}
                 style={{ objectFit: "contain", width: "auto", height: "685px", borderRadius: "20px" }}
               />
             </Box>
@@ -79,7 +79,7 @@ export const ArcCard: React.FC = () => {
                 Schedule games without the endless back-and-forth
               </Typography>
 
-              <Typography color="text.secondary" variant="h6" sx={{ maxWidth: "520px" }}>
+              <Typography color="text.secondary" variant="h6" sx={{ maxWidth: "620px" }}>
                 Reduce the endless back-and-forth with spreadsheets, emails or athletic departments and cut your scheduling time in half. Sync your spreadsheets with your calendars, send out multiple
                 mass emails and stay organized with our other features.
               </Typography>
